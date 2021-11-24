6c37dc00e267629c9ed472a9023a144b38339845
Chernenko Artem <a.chernenko@staffcop.ru>
2021-11-24 14:33:42 +0700

    Task 3: Create new class for users params

--- TestCreateUser.py
+++ TestCreateUser.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from user import User
 
 
 class TestCreateUser():
@@ -16,11 +17,12 @@ class TestCreateUser():
         self.open_home_page()
         self.login(username="admin", password="secret")
         self.open_users_page()
-        self.create_user(firstname="Eva", middlename="Semenova", lastname="Cocs", nickname="Coca", title="Nocomments",
-                         company="Google", address="Russia", home="Moscow", mobile="169421+", work="Google",
-                         fax="14481561", email="eva@mail.ru", email2="no", email3="no", homepage="no",
-                         phone2="+89945669", bday=12, bmounth="'October'", byear="1991", aday="'25'",
-                         amonth="'November'", ayear="2015", address2="Rome", notes="No")
+        send_test_user_params = User(firstname="Eva", middlename="Semenova", lastname="Cocs", nickname="Coca", title="Nocomments",
+                                     company="Google", address="Russia", home="Moscow", mobile="169421+", work="Google",
+                                     fax="14481561", email="eva@mail.ru", email2="no", email3="no", homepage="no",
+                                     phone2="+89945669", bday=12, bmounth="'October'", byear="1991", aday="'25'",
+                                     amonth="'November'", ayear="2015", address2="Rome", notes="No")
+        self.create_user(send_test_user_params)
         self.logout()
         self.close_home_page()
 
@@ -30,68 +32,66 @@ class TestCreateUser():
     def logout(self):
         self.driver.find_element(By.LINK_TEXT, "Logout").click()
 
-    def create_user(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
-                    email, email2, email3, homepage, phone2, bday, bmounth, byear, aday, amonth, ayear, address2,
-                    notes):
+    def create_user(self, user):
         self.driver.find_element(By.NAME, "firstname").click()
-        self.driver.find_element(By.NAME, "firstname").send_keys(firstname)
+        self.driver.find_element(By.NAME, "firstname").send_keys(user.firstname)
         self.driver.find_element(By.NAME, "middlename").click()
-        self.driver.find_element(By.NAME, "middlename").send_keys(middlename)
+        self.driver.find_element(By.NAME, "middlename").send_keys(user.middlename)
         self.driver.find_element(By.NAME, "lastname").click()
-        self.driver.find_element(By.NAME, "lastname").send_keys(lastname)
+        self.driver.find_element(By.NAME, "lastname").send_keys(user.lastname)
         self.driver.find_element(By.NAME, "nickname").click()
-        self.driver.find_element(By.NAME, "nickname").send_keys(nickname)
+        self.driver.find_element(By.NAME, "nickname").send_keys(user.nickname)
         self.driver.find_element(By.NAME, "title").click()
         self.driver.find_element(By.NAME, "title").click()
-        self.driver.find_element(By.NAME, "title").send_keys(title)
+        self.driver.find_element(By.NAME, "title").send_keys(user.title)
         self.driver.find_element(By.NAME, "company").click()
-        self.driver.find_element(By.NAME, "company").send_keys(company)
+        self.driver.find_element(By.NAME, "company").send_keys(user.company)
         self.driver.find_element(By.NAME, "address").click()
-        self.driver.find_element(By.NAME, "address").send_keys(address)
+        self.driver.find_element(By.NAME, "address").send_keys(user.address)
         self.driver.find_element(By.NAME, "home").click()
-        self.driver.find_element(By.NAME, "home").send_keys(home)
+        self.driver.find_element(By.NAME, "home").send_keys(user.home)
         self.driver.find_element(By.NAME, "mobile").click()
-        self.driver.find_element(By.NAME, "mobile").send_keys(mobile)
+        self.driver.find_element(By.NAME, "mobile").send_keys(user.mobile)
         self.driver.find_element(By.NAME, "work").click()
-        self.driver.find_element(By.NAME, "work").send_keys(work)
+        self.driver.find_element(By.NAME, "work").send_keys(user.work)
         self.driver.find_element(By.NAME, "fax").click()
-        self.driver.find_element(By.NAME, "fax").send_keys(fax)
+        self.driver.find_element(By.NAME, "fax").send_keys(user.fax)
         self.driver.find_element(By.NAME, "email").click()
-        self.driver.find_element(By.NAME, "email").send_keys(email)
+        self.driver.find_element(By.NAME, "email").send_keys(user.email)
         self.driver.find_element(By.NAME, "email2").click()
-        self.driver.find_element(By.NAME, "email2").send_keys(email2)
+        self.driver.find_element(By.NAME, "email2").send_keys(user.email2)
         self.driver.find_element(By.NAME, "email3").click()
         self.driver.find_element(By.NAME, "email3").click()
-        self.driver.find_element(By.NAME, "email3").send_keys(email3)
+        self.driver.find_element(By.NAME, "email3").send_keys(user.email3)
         self.driver.find_element(By.NAME, "homepage").click()
         self.driver.find_element(By.NAME, "homepage").click()
-        self.driver.find_element(By.NAME, "homepage").send_keys(homepage)
+        self.driver.find_element(By.NAME, "homepage").send_keys(user.homepage)
         self.driver.find_element(By.NAME, "bday").click()
         dropdown = self.driver.find_element(By.NAME, "bday")
-        dropdown.find_element(By.XPATH, f"//option[. = {bday}]").click()
+        dropdown.find_element(By.XPATH, f"//option[. = {user.bday}]").click()
         self.driver.find_element(By.CSS_SELECTOR, "select:nth-child(61) > option:nth-child(14)").click()
         self.driver.find_element(By.NAME, "bmonth").click()
         dropdown = self.driver.find_element(By.NAME, "bmonth")
-        dropdown.find_element(By.XPATH, "//option[. = %s]" % bmounth).click()
+        dropdown.find_element(By.XPATH, "//option[. = %s]" % user.bmounth).click()
         self.driver.find_element(By.CSS_SELECTOR, "select:nth-child(62) > option:nth-child(11)").click()
         self.driver.find_element(By.NAME, "byear").click()
-        self.driver.find_element(By.NAME, "byear").send_keys(byear)
+        self.driver.find_element(By.NAME, "byear").send_keys(user.byear)
         self.driver.find_element(By.NAME, "aday").click()
         dropdown = self.driver.find_element(By.NAME, "aday")
-        dropdown.find_element(By.XPATH, "//option[. = %s]" % aday).click()
+        dropdown.find_element(By.XPATH, "//option[. = %s]" % user.aday).click()
         self.driver.find_element(By.CSS_SELECTOR, "select:nth-child(66) > option:nth-child(27)").click()
         self.driver.find_element(By.NAME, "amonth").click()
         dropdown = self.driver.find_element(By.NAME, "amonth")
-        dropdown.find_element(By.XPATH, "//option[. = %s]" % amonth).click()
+        dropdown.find_element(By.XPATH, "//option[. = %s]" % user.amonth).click()
         self.driver.find_element(By.CSS_SELECTOR, "select:nth-child(67) > option:nth-child(12)").click()
         self.driver.find_element(By.NAME, "ayear").click()
-        self.driver.find_element(By.NAME, "ayear").send_keys(ayear)
+        self.driver.find_element(By.NAME, "ayear").send_keys(user.ayear)
         self.driver.find_element(By.NAME, "address2").click()
-        self.driver.find_element(By.NAME, "address2").send_keys(address2)
+        self.driver.find_element(By.NAME, "address2").send_keys(user.address2)
         self.driver.find_element(By.NAME, "phone2").click()
-        self.driver.find_element(By.NAME, "phone2").send_keys(phone2)
+        self.driver.find_element(By.NAME, "phone2").send_keys(user.phone2)
         self.driver.find_element(By.NAME, "notes").click()
-        self.driver.find_element(By.NAME, "notes").send_keys(notes)
+        self.driver.find_element(By.NAME, "notes").send_keys(user.notes)
         self.driver.find_element(By.NAME, "theform").click()
         self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(87)").click()
 
